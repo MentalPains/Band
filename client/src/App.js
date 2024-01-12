48f537abdc8c6206e1ca625377ae15e83bd5e97d
mentalpains <johnathon93639@gmail.com>
2024-01-12 16:40:26 +0800

    Fully Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import React from "react";
-
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
-import RecordList from "./components/telephonelist";
-
-// We import all the components we need in our app
-import Navbar from "./components/navbar";
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import RecordList from './components/telephoneList';
+import Create from './components/create';
+import Edit from './components/edit';
+import Navbar from './components/navbar';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<RecordList />} />
+        <Route path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
     </div>
   );
