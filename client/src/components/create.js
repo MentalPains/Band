ca20478df314ce4f762a2e163ea0429b3aa7227b
mentalpains <johnathon93639@gmail.com>
2024-01-12 16:28:55 +0800

    Completed UI testing and front-end code coverage

---
+++ client/src/components/create.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Create() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+  const navigate = useNavigate();
+
+  // This method will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newPerson = { ...form };
+
+    try {
+      await fetch("http://localhost:5050/record", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPerson),
+      });
+
+      // Reset the form and navigate to the root page.
+      setForm({ name: "", email: "", mobile: "" });
+      navigate("/");
+    } catch (error) {
+      window.alert(error);
+      return;
+    }
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div>
+      <h3>Create New Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={form.email}
+            onChange={(e) => updateForm({ email: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobile">Mobile</label>
+          <input
+            type="text"
+            className="form-control"
+            id="mobile"
+            value={form.mobile}
+            onChange={(e) => updateForm({ mobile: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Person"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
